e986ba749052d728ff97012133d8ddd32e0d7377
hyperion9913 <supernoob7903@gmail.com>
2025-03-24 16:33:55 +0530

    img

--- src/App.jsx
+++ src/App.jsx
@@ -14,9 +14,9 @@ const App = () => {
 
       <main className="flex-grow p-4">
         <div className="img-sec flex flex-wrap justify-around gap-4 mt-3">
-          <img src={"/src/assets/img/bgImg1.jpg"} alt="Background 1" className="bgimg h-[545px] rounded-lg shadow-lg" />
-          <img src="./src/assets/img/bgImg2.jpg" alt="Background 2" className="bgimg h-[545px] rounded-lg shadow-lg" />
-          <img src="./src/assets/img/bgImg3.jpg" alt="Background 3" className="bgimg h-[545px] rounded-lg shadow-lg" />
+          <img src="https://dg4f756elm.ufs.sh/f/G7qZPEF3toKqi3OgOcERcDehzW35rdASL0Gy9bHNKTfBvJFQ" alt="Background 1" className="bgimg h-[545px] rounded-lg shadow-lg" />
+          <img src="https://dg4f756elm.ufs.sh/f/G7qZPEF3toKqLUGeaD4pvNxlRhzTfQbOjZYF6d87AXIn1qtV" alt="Background 2" className="bgimg h-[545px] rounded-lg shadow-lg" />
+          <img src="https://dg4f756elm.ufs.sh/f/G7qZPEF3toKqWhn5ssRzgGAYXrQzZTNa4poUFyH1hOwuPI85" alt="Background 3" className="bgimg h-[545px] rounded-lg shadow-lg" />
         </div>
 
         <BookSlider />
