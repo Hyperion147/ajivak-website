442dff36907ada660508b3ce7ee7fc89826ee8ca
Hyperion147 <supernoob7903@gmail.com>
2025-05-28 22:01:08 +0530

    testing router in deployment

--- src/App.jsx
+++ src/App.jsx
@@ -1,23 +1,22 @@
-import { Routes, Route } from 'react-router-dom'
+import { BrowserRouter as Router, Routes, Route } from 'react-router-dom';
 import Update from './components/Update';
 import ArticlePage from './components/pages/ArticlePage';
 import Literature from './components/pages/Literature';
 import Header from './components/Header';
-import Footer from './components/Footer';
 
 const App = () => {
   return (
-    <div className="font-roboto bg-background text-text min-h-screen flex flex-col">
-      <Header />
-      <Routes>
-        <Route path="/" element={
-          <Update />
-        } />
-        
-        <Route path="/articles/:id" element={<ArticlePage />} />
-        <Route path="/literature" element={<Literature />} />
-      </Routes>
-    </div>
+    <Router>
+      <div className="font-roboto bg-background text-text min-h-screen flex flex-col">
+        <Header />
+        <Routes>
+          <Route path="*" element={<Update />} />
+          <Route path="/" element={<Update />} />
+          <Route path="/articles/:id" element={<ArticlePage />} />
+          <Route path="/literature" element={<Literature />} />
+        </Routes>
+      </div>
+    </Router>
   );
 };
 
